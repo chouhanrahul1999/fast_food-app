11dbafca5a0bf3876c95431025340b50addd8e43
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-09 22:39:52 +0530

    creatd appwrite authe

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.rahul.fast_food",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
