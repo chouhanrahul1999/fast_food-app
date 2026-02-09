b4a7644008223957c7db2dc3d02596bfd2386966
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-10 00:59:56 +0530

    creatd input box component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,55 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Dimensions,
+  ImageBackground,
+  Image,
+} from "react-native";
 import React from "react";
 import { Slot } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import InputBox from "@/components/InputBox";
+import Button from "@/components/Button";
+import { text } from "express";
 
 export default function _layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth Layout</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+
+        <InputBox
+          placeholder="Enter Your email"
+          label="Email"
+          onChangeText={(text) => {}}
+          value=""
+          keyboardType="email-address"
+        />
+        <Button />
+      </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.rahul.fast_food",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID
 };
