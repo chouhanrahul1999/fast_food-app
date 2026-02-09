09e43f2bb49a5c8f83f824b94b7840407c9e2dfb
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-10 01:13:47 +0530

    creatd custom button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import React from "react";
+import { CustomButtonProps } from "@/type";
+import cn from "clsx";
 
-const Button = () => {
+const Button = ({
+  onPress,
+  title = "Click Me",
+  style,
+  textStyle,
+  leftIcon,
+  isLoading = false,
+}: CustomButtonProps) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
-  )
-}
+    <TouchableOpacity className={cn("custom-btn", style)} onPress={onPress}>
+      {leftIcon}
 
-export default Button
+      <View className="flex-center flex-row">
+        {isLoading ? (
+          <ActivityIndicator size={"small"} color={"white"} />
+        ) : (
+          <Text className={cn("text-white-100 paragraph-semibold", textStyle)}>
+            {title}
+          </Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
